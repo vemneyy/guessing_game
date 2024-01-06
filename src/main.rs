4cb6c31e53f5125bc320561d7924f4ae29892207
sonoma <wllamsmth1@gmail.com>
2024-01-06 21:52:34 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Угадай число!");
 
-    let random_number:i8 = rand::thread_rng()
+    let random_number:u8 = rand::thread_rng()
         .gen_range(1..=10);
 
-    println!("Укажите своё предположение: ");
+    loop {
+        println!("\nУкажите своё предположение: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Не удалось считать значение");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Не удалось считать значение");
 
-    println!("Вы предположили: {guess}");
-    println!("Случайное число: {}", &random_number);
+        let guess: u8 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("Введите число от 1 до 10.");
+                continue;
+            },
+        };
+
+        println!("Вы предположили: {guess}");
+        // println!("Случайное число: {}", &random_number);
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Вы выбрали меньше."),
+            Ordering::Greater => println!("Вы выбрали больше."),
+            Ordering::Equal => {
+                println!("Вы угадали! Числом было {}.", random_number);
+                break;
+            }
+        }
+    }
 }
